0631802f244e9901c49dc9b1619d0899103a96e5
zzzssc <1556837456@qq.com>
2019-06-13 11:55:45 +0800

    我的第一个程序
    输出俩个数

--- tastttt/main.cpp
+++ tastttt/main.cpp
@@ -1,15 +1,41 @@
 //
 //  main.cpp
-//  tastttt
+//  testCPP
 //
 //  Created by s20181106119 on 2019/6/13.
 //  Copyright © 2019 s20181106119. All rights reserved.
 //
 
 #include <iostream>
+using namespace std;
+
+// insert code here...
+class point {
+public:
+    int a,b;
+    point (int x=0,int y=0):a(x),b(y){
+        x=a;
+        y=b;
+    }
+    void get(){
+        cin>>x>>y;
+    }
+    void display()
+    {
+        cout<<x<<" "<<y;
+    }
+private:
+    int x,y;
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    point p;
+    p.get();
+    p.display();
     return 0;
+    
 }
+//std::cout << "Hello, World!\n";
+//return 0;
+
